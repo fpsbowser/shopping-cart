2768c5dba004b804964e3a8253b8851807f0b554
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-19 15:47:25 -0400

    initial routes commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Navigation from "./components/Navigation";
+import "./styles/app.css";
+import products from "./data/Products";
 
 function App() {
-  return <div className="App">Test</div>;
+  console.log(products);
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop products={products} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
