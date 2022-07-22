15cb1f88dd60ef21ab6aa1cc492797bba9088f62
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-21 20:10:02 -0400

    added route to homepage, props, and state hook

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/navigation.css";
-import Cart from "./Cart";
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const { quantity } = props;
+
+  console.log(quantity);
+
   return (
     <nav>
-      <h3>Logo</h3>
+      <Link to={"/"}>
+        <img
+          src={require("../assets/backwards-e-embalelem.jpg")}
+          alt="logo"
+          id="logo"
+        />
+      </Link>
       <Link to="/shop">
         <h2>Shop</h2>
       </Link>
       <Link to="/about">
         <h2>About</h2>
       </Link>
-      <Cart />
+      <Link to="/cart">
+        <div id="cart-container">
+          <span className="material-symbols-outlined">shopping_bag</span>
+          <p className="number-of-items">{quantity}</p>
+        </div>
+      </Link>
     </nav>
   );
 };
