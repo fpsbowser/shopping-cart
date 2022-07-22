81fd857cebbe41bceb61ab8afbbee8af2d2439b0
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-21 20:07:10 -0400

    added background to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles/home.css";
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div id="container">
+      <img
+        src={require("../assets/Eminem-background.jpg")}
+        alt="eminem-background"
+        id="eminem-background"
+      />
+      <div className="center">
+        <Link to={"/shop"}>
+          <button id="enter-btn">Enter</button>
+        </Link>
+      </div>
     </div>
   );
 };
