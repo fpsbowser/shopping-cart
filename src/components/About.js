3df03fcaeba9f764b894f5e9bbf051cfaa0f426b
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-07-21 20:06:49 -0400

    added information to render

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../styles/about.css";
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1 id="about-title">About Page</h1>
+      <p>
+        Following theodinproject curriculum, for the shopping cart project, I
+        have decided to make a replica of
+        <a id="eminem-link" href="https://shop.eminem.com/collections/kamikaze">
+          {" "}
+          Eminems Merch Store
+        </a>
+        . <br></br>
+        You can't actually buy anything from this website!
+        <br></br>
+        All assets are straight from his store. I do not own the rights to any
+        of the assets.
+      </p>
     </div>
   );
 };
